bbd33be86ac5b6aedc7e350f6d9ba290302fc389
ilhanhutta649 <ilhanhutta@gmail.com>
2025-11-27 23:36:32 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e  # stop if any script fails
+
+echo "=== Running Logistic Regression ==="
+python src/logistic_regression.py
+
+echo "=== Running Random Forest ==="
+python src/random_forest.py
+
+echo "=== Running XGBoost Model ==="
+python src/xgb_model.py
