bda7338075aa97c94089b2e706d64f53aa2ff9a4
231448m-don <anonymousbrandon3756@gmail.com>
2025-11-30 19:28:12 +0800

    Delete kedro_pipeline directory

--- kedro_pipeline/src/kedro/__init__.py
+++
@@ -1,4 +0,0 @@
-"""kedro_pipeline
-"""
-
-__version__ = "0.1"
--- kedro_pipeline/src/kedro/pipeline_registry.py
+++
@@ -1,9 +0,0 @@
-from kedro.framework.project import find_pipelines
-from kedro.pipeline import Pipeline
-from .pipelines.models.pipeline import create_pipeline as models_pipeline
-
-def register_pipelines() -> dict[str, Pipeline]:
-    return {
-        "models": models_pipeline(),
-        "__default__": models_pipeline(),
-    }
