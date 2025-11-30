511dee7413a99ce86d95ca3f8a40d8c3a52ecd7e
231448m-don <anonymousbrandon3756@gmail.com>
2025-11-30 16:39:53 +0800

    Update pipeline.py

--- kedro_pipeline/src/kedro/pipelines/models/pipeline.py
+++ kedro_pipeline/src/kedro/pipelines/models/pipeline.py
@@ -1,31 +1,28 @@
 from kedro.pipeline import Pipeline, node
-from .logistic_regression import (
-    preprocess_data,
-    train_logistic_regression,
-    evaluate_model
-)
+from .logistic_regression import train_logistic_regression
+from .random_forest import train_random_forest
+from .xgboost import train_xgboost
 
 def create_pipeline(**kwargs):
-    return Pipeline([
-        node(
-            func=preprocess_data,
-            inputs=dict(
-                df="raw_model_data",
-                target_column="params:target_column"
+    return Pipeline(
+        [
+            node(
+                func=train_logistic_regression,
+                inputs="model_input_data",
+                outputs="logistic_model_output",
+                name="logistic_regression_node"
             ),
-            outputs=["X", "y"],
-            name="preprocess_data"
-        ),
-        node(
-            func=train_logistic_regression,
-            inputs=["X_train", "y_train"],
-            outputs="logistic_model",
-            name="train_logistic_regression"
-        ),
-        node(
-            func=evaluate_model,
-            inputs=["logistic_model", "X_test", "y_test"],
-            outputs="logistic_metrics",
-            name="evaluate_logistic_model"
-        ),
-    ])
+            node(
+                func=train_random_forest,
+                inputs="model_input_data",
+                outputs="random_forest_model_output",
+                name="random_forest_node"
+            ),
+            node(
+                func=train_xgboost,
+                inputs="model_input_data",
+                outputs="xgboost_model_output",
+                name="xgboost_node"
+            ),
+        ]
+    )
